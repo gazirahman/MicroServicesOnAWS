805f62594281ea22b8111f75a895d03fa02f0364
Gazi Rahman <mushfiq@gazirahman.com>
2022-01-10 23:44:33 -0500

    My AWS Profile is configured for Photos App (Deploymen/API Access)

--- infra/main.tf
+++ infra/main.tf
@@ -9,8 +9,8 @@
 # https://www.terraform.io/docs/providers/aws/index.html
 # select the region you want to perform this operation in. 
 provider "aws" {
-  profile = "cr" 
-  region = "us-west-2"
+  profile = "photos" 
+  region = "us-east-1"
 }
 
 resource "aws_dynamodb_table" "dynamoUsers" {
